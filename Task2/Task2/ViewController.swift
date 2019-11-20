ab08de747079b7ea367645a610d239db0694770e
kmizuno1 <kmizuno@appdevteam.work>
2019-11-20 15:00:32 +0900

    first commit

---
+++ Task2/Task2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task2
+//
+//  Created by k.mizuno@appdevteam.work on 2019/11/20.
+//  Copyright © 2019 k.mizuno@appdevteam.work. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
