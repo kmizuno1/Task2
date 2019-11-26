e49c1b344c40d6cb72a55b0fc7042df9f2b5bdec
kmizuno1 <kmizuno@appdevteam.work>
2019-11-26 19:12:19 +0900

    #3_ログインボタン、ログアウトボタンそれぞれをisHiddenで判定するようにした 初期設定をsetupView()にまとめた

--- Task2/Task2/CustomView.swift
+++ Task2/Task2/CustomView.swift
@@ -18,31 +18,7 @@ class CustomView: UIView {
         
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        //パーツの情報を設定
-        //ラベル1
-        label_loginDate.text = "前回ログイン日時 2019/11/07 12:00:00"
-        label_loginDate.font = UIFont.systemFont(ofSize: 16)
-        //ボタン1
-        button_login.setTitle("ログイン", for: .normal)
-        button_login.setTitleColor(.blue, for: .normal)
-        button_login.titleLabel?.font = UIFont.systemFont(ofSize: 16)
-        button_login.isHidden = false
-        button_login.addTarget(self, action: #selector(loginButtonEvent(_:)), for: .touchUpInside)
-        //ボタン2
-        button_logout.setTitle("ログアウト", for: .normal)
-        button_logout.setTitleColor(.blue, for: .normal)
-        button_logout.titleLabel?.font = UIFont.systemFont(ofSize: 16)
-        button_logout.isHidden = true
-        button_logout.addTarget(self, action: #selector(logoutButtonEvent(_:)), for: .touchUpInside)
-        //ボタン3
-        button_setting.setTitle("各種設定", for: .normal)
-        button_setting.setTitleColor(.blue, for: .normal)
-        button_setting.titleLabel?.font = UIFont.systemFont(ofSize: 16)
-        //スイッチ1
-        labelSwitch._label.text = "自動ログイン"
-        labelSwitch._label.font = UIFont.systemFont(ofSize: 16)
-        labelSwitch._switch.isOn = false
+        setupView()
         
         //パーツを追加
         addSubview(label_loginDate)
@@ -56,32 +32,57 @@ class CustomView: UIView {
         super.init(coder: aDecoder)
     }
     
+    func setupView(){
+      //パーツの情報を設定
+      //各種設定ボタン
+      button_setting.setTitle("各種設定", for: .normal)
+      button_setting.setTitleColor(.blue, for: .normal)
+      button_setting.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+      //ログインボタン
+      button_login.setTitle("ログイン", for: .normal)
+      button_login.setTitleColor(.blue, for: .normal)
+      button_login.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+      button_login.isHidden = false
+      button_login.addTarget(self, action: #selector(loginButtonEvent(_:)), for: .touchUpInside)
+      //ログアウトボタン
+      button_logout.setTitle("ログアウト", for: .normal)
+      button_logout.setTitleColor(.blue, for: .normal)
+      button_logout.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+      button_logout.isHidden = true
+      button_logout.addTarget(self, action: #selector(logoutButtonEvent(_:)), for: .touchUpInside)
+      //自動ログイン
+      labelSwitch._label.text = "自動ログイン"
+      labelSwitch._label.font = UIFont.systemFont(ofSize: 16)
+      labelSwitch._switch.isOn = false
+      //ラベル
+      label_loginDate.text = "前回ログイン日時 2019/11/07 12:00:00"
+      label_loginDate.font = UIFont.systemFont(ofSize: 16)
+      }
+    
     override func layoutSubviews() {
         super.layoutSubviews()
-        
-        let labelSize = label_loginDate.sizeThatFits(label_loginDate.frame.size)
-        let loginSize = button_login.sizeThatFits(button_login.frame.size)
-        let logoutSize = button_logout.sizeThatFits(button_logout.frame.size)
-        let settingSize = button_setting.sizeThatFits(button_setting.frame.size)
-        let labelSwitchSize = labelSwitch.sizeThatFits(labelSwitch.frame.size)
-        var x = self.frame.width - safeAreaInsets.right - 10 - settingSize.width
-        let y = safeAreaInsets.top
-        var n: CGFloat
-        
         //ボタンとラベルのframeをセット
-        button_setting.frame = CGRect(x: x, y: y, width: settingSize.width, height: settingSize.height)
-        if button_login.isHidden {
-            n = logoutSize.width
-        }else{
-            n = loginSize.width
+        var x = self.frame.width - safeAreaInsets.right
+        let y = safeAreaInsets.top
+        //設定ボタン
+        x = x  - 10 - button_setting.sizeThatFits(button_setting.frame.size).width
+        button_setting.frame = CGRect(x: x, y: y, width: button_setting.sizeThatFits(button_setting.frame.size).width, height: button_setting.sizeThatFits(button_setting.frame.size).height)
+        //ログインボタン
+        if !button_login.isHidden {
+            x = x - 10 - button_login.sizeThatFits(button_login.frame.size).width
+            button_login.frame = CGRect(x: x, y: y, width: button_login.sizeThatFits(button_login.frame.size).width, height: button_login.sizeThatFits(button_login.frame.size).height)
         }
-        x = x - 10 - n
-        button_logout.frame = CGRect(x: x, y: y, width: logoutSize.width, height: logoutSize.height)
-        button_login.frame = CGRect(x: x, y: y, width: loginSize.width, height: loginSize.height)
-        x = x - 10 - labelSwitchSize.width
-        labelSwitch.frame = CGRect(x: x, y: y, width: labelSwitchSize.width, height: labelSwitchSize.height)
+        //ログアウトボタン
+        if !button_logout.isHidden {
+            x = x - 10 - button_logout.sizeThatFits(button_logout.frame.size).width
+            button_logout.frame = CGRect(x: x, y: y, width: button_logout.sizeThatFits(button_logout.frame.size).width, height: button_logout.sizeThatFits(button_logout.frame.size).height)
+        }
+        //スイッチ
+        x = x - 10 - labelSwitch.sizeThatFits(labelSwitch.frame.size).width
+        labelSwitch.frame = CGRect(x: x, y: y, width: labelSwitch.sizeThatFits(labelSwitch.frame.size).width, height: labelSwitch.sizeThatFits(labelSwitch.frame.size).height)
+        //ラベル
         x = x - 20
-        label_loginDate.frame = CGRect(x: 10 + safeAreaInsets.left, y: y + 6, width: x, height: labelSize.height)
+        label_loginDate.frame = CGRect(x: safeAreaInsets.left + 10, y: y + 6, width: x, height: label_loginDate.sizeThatFits(label_loginDate.frame.size).height)
     }
     
     //ログインボタンタップイベント
